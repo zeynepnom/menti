1f0ccfebc7c758f0a556a739d969e8c6d33bd136
zeynepnom <zeynepnom1223@gmail.com>
2025-05-24 17:12:53 +0300

    Veri URL'leri eklendi

---
+++ data-sources.py
@@ -0,0 +1,4 @@
+DATASETS = {
+    'studentlife': 'https://studentlife.cs.dartmouth.edu/data/survey.csv',
+    'mhealth': 'https://archive.ics.uci.edu/ml/machine-learning-databases/00319/MHEALTHDATASET.zip'
+}
