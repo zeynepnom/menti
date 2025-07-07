1370c6755f7d73bd515dbb3fa1ab0a23d55fcf5a
zeynepnom <144964379+zeynepnom@users.noreply.github.com>
2025-07-07 21:55:56 +0300

    Update data-sources.py

--- data-sources.py
+++ data-sources.py
@@ -1,4 +1,1 @@
-DATASETS = {
-    'studentlife': 'https://studentlife.cs.dartmouth.edu/data/survey.csv',
-    'mhealth': 'https://archive.ics.uci.edu/ml/machine-learning-databases/00319/MHEALTHDATASET.zip'
-}
+
